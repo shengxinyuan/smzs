648e7a318d7bc230ead43da45f4dd41cfe1d8e63
shengxinyuan <461415703@qq.com>
2022-01-15 15:28:13 +0800

    feat 商品列表

--- pages/set-shop/goods-list.vue
+++ pages/set-shop/goods-list.vue
@@ -1,6 +1,66 @@
 <template>
 	<view>
-		商品列表
+		<u-tabs ref="tabs" :is-scroll="true" :list="firstList" active-color="#2979ff" inactive-color="#606266" font-size="30" :current="first" @change="changeFirst"></u-tabs>
+		<view style="padding-top: 200rpx;" v-if="shop_list.length === 0">
+			<u-empty text="暂无商品" mode="list"></u-empty>
+		</view>
+		<scroll-view scroll-y="true" class="cont_list_two">
+			<view class="cont_item" v-for="(item,i) in shop_list" :key="i">
+				<image class="images" :src="item.image" mode="aspectFill"></image>
+				<view class="base-cont">
+					<view class="title">
+						{{item.title}}
+					</view>
+					<view class="it_selt_l">
+						<text><text style="">￥</text>{{item.price}}</text>
+					</view>
+				</view>
+				<view class="index-cont">
+					<view v-if="first === 0">
+						<view class="index-btn" @click="editIndex(item)">
+							编辑
+						</view>
+						<view class="index-btn" @click="editIndex(item)">
+							下架
+						</view>
+					</view>
+					
+					<view v-if="first === 1">
+						<view class="index-btn" @click="editIndex(item)">
+							编辑
+						</view>
+						<view class="index-btn" @click="editIndex(item)">
+							删除
+						</view>
+					</view>
+					
+					<view v-if="first === 2">
+						<view class="index-btn" @click="editIndex(item)">
+							上架
+						</view>
+						<view class="index-btn" @click="editIndex(item)">
+							编辑
+						</view>
+						<view class="index-btn" @click="editIndex(item)">
+							删除
+						</view>
+					</view>
+					
+					<view v-if="first === 3">
+						<view class="index-btn" @click="editIndex(item)">
+							撤回
+						</view>
+					</view>
+					
+					<view v-if="first === 4">
+						<view class="index-btn" @click="editIndex(item)">
+							编辑
+						</view>
+					</view>
+					
+				</view>
+			</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -8,22 +68,98 @@
 	export default {
 		data() {
 			return {
-				upload_url: 'https://zuanshi.semoh.cn/api/uploads',
-				image_plan: '',
-				header: {
-					'token': uni.getStorageSync('token')
-				}
+				shop_list: [],
+				firstList: [
+					{name: '在售'},
+					{name: '草稿箱'},
+					{name: '已下架'},
+					{name: '审核中'},
+					{name: '审核拒绝'},
+				],
+				secondList: [],
+				first: 0,
+				second: 0,
+				editShow: false
 			}
+		},
+		onLoad () {
+			this.shop_list = [
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+				{
+					title: '商品1',
+					sale: 10,
+					price: 1000,
+					index: 1,
+					image: 'http://zuanshi.semoh.cn/uploads/20210825/80552891ab2b03b711f359a2580e02f3.jpg',
+				},
+			];
 		},
 		methods: {
 			// 上传店铺照片
-			result_plan(e) {
-				console.log(e)
-				if (e.status == 1) {
-					this.image_plan = e.data
-				} else {
-					console.log(e.message)
-				}
+			editIndex(item) {
+				this.editShow = true;
 			},
 			// 上传照片删除
 			remove(i) {
@@ -48,22 +184,104 @@
 						}, 1500);
 					}
 				})
-			}
+			},
+			changeFirst (index) {
+				this.first = index;
+			},
 		}
 	}
 </script>
 
-<style>
-	.save_btn{
-		padding: 30rpx;
-		margin-top: 20rpx;
+<style lang="scss" scoped>
+	.cont_list_two {
+		width: 100%;
+		border-top: 1px solid #eee;
+		.cont_item {
+			width: 100%;
+			border-radius: 16rpx;
+			background-color: white;
+			overflow: hidden;
+			display: flex;
+			align-items: center;
+			border-bottom: 1px solid #eee;
+			.images {
+				width: 180rpx;
+				border-radius: 10rpx;
+				height: 180rpx;
+				display: block;
+			}
+			
+			.base-cont {
+				flex: 1;
+				font-size: 24rpx;
+				.title {
+					font-size: 36rpx;
+					margin-bottom: 8rpx;
+				}
+			}
+			.index-cont {
+				width: 100rpx;
+				font-size: 24rpx;
+				.index-btn {
+					margin-top: 10px;
+					color: #2979ff;
+				}
+			}
+	
+			.it_text {
+				width: 100%;
+				.it_tit {
+					font-size: 28rpx;
+					width: 96%;
+					overflow: hidden;
+				}
+	
+				.it_text_cen {
+					
+	
+					.it_price {
+						font-size: 30rpx;
+					}
+	
+					.it_selt_l {
+						display: flex;
+	
+						text {
+							color: #ea5b72;
+							font-size: 30rpx;
+						}
+	
+						image {
+							width: 60rpx;
+							height: 24rpx;
+							margin-top: 10rpx;
+						}
+					}
+				}
+	
+				.it_label {
+					width: 100%;
+					display: flex;
+					margin-top: 10rpx;
+					margin-bottom: 20rpx;
+	
+					view {
+						font-size: 22rpx;
+						padding: 0 8rpx;
+						border: 2rpx solid #DD524D;
+						color: #DD524D;
+						border-radius: 6rpx;
+					}
+				}
+	
+				.it_selt_r {
+					color: #998;
+					margin-top: 6rpx;
+					font-size: 24rpx;
+				}
+			}
+	
+		}
 	}
-	.save_btn button {
-		font-size: 30upx;
-		color: #FFFFFF;
-		background-image: linear-gradient(90deg,
-				#2b3f7d 0%,
-				#1b2c60 100%);
-		border-radius: 40upx;
-	}
+	
 </style>
