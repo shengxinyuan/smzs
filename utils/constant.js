3cd40c443187dd6e8e595ad084b6acfc56b28b13
shengxinyuan <461415703@qq.com>
2022-03-09 15:09:58 +0800

    feat 3DURL 按环境区分

--- utils/constant.js
+++ utils/constant.js
@@ -1,6 +1,6 @@
-import { get } from '../api'
+import { get, env } from '../api'
 
-let THREE_D_H5_URL = 'http://3d.semoh.cn/'
+let THREE_D_H5_URL = env === 'prod' ? 'http://3d.semoh.cn/' : 'http://test-3d.semoh.cn/'
 
 const getThreeDesignH5Url = () => {
 	get('3d/geth5url').then((res) => {
