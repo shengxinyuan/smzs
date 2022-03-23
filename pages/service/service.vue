31ddcd167ed7fd70114e124cb2e2c12c38c90a01
shengxinyuan <461415703@qq.com>
2022-03-23 11:27:12 +0800

    feat 客服改成企业微信二维码

--- pages/service/service.vue
+++ pages/service/service.vue
@@ -1,6 +1,8 @@
 <template>
-	<view>
-		<web-view src="http://kefu.nxm.wanheweb.com/addons/kefu/index/mobile?fixed_csr=6"></web-view>
+	<view class="kefu">
+		<image class="img" :src="img" mode="widthFix"></image>
+		<button type="default" @click="saveImage">保存客服二维码</button>
+		<view class="tip">微信内扫描二维码就能联系客服</view>
 	</view>
 </template>
 
@@ -8,15 +10,38 @@
 	export default {
 		data() {
 			return {
-				
+				img: 'https://sermer-app-oss.oss-cn-hangzhou.aliyuncs.com/front-end-static/kefu.jpg'
 			}
 		},
 		methods: {
-			
+			saveImage() {
+				uni.saveImageToPhotosAlbum({
+					filePath: this.img,
+					success(res) {
+						uni.showToast({
+							title: '已保存到相册！',
+							icon: 'success',
+							duration: 2000
+						})
+					}
+				})
+			},
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.kefu {
+	padding: 80rpx 30rpx;
+}
+.img {
+	display: block;
+	margin: 0 auto;
+}
+.tip {
+	text-align: center;
+	margin-top: 20rpx;
+	color: #f00;
+	font-size: 24rpx;
+}
 </style>
